e5a531f658e1ad34dfcbf92e8ff5ba89c5ee4d20
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-03-01 17:41:57 +0100

    avanzamento

--- js/main.js
+++ js/main.js
@@ -1,39 +1,45 @@
 const mine = 16;
 
 var mineArray = [];
-var oneToHun = [];
-var ggArray = [];
+
 
 
 flowers(mine);
-oneHun(1, 100)
-oneToHun.pop(mineArray)
 console.log(mineArray);
-console.log(oneToHun);
+
 
 function myBtn(){
     for ( i = 0; i < 1; i++){
         utnNum = parseInt(prompt("Inserisci un numero da 1 a 100"));
-        if (utnNum != mineArray) {
+        if (arrayContain(utnNum, mineArray)) {
             alert("Hai perso!")
         } else {
-            ggArray.push(utnNum);
             alert("Nessuna Mina")
         }
     }
 }
 
 
-function flowers(sixTeen) {
-    for ( i = 1; i < sixTeen + 1; i++){
-        mineArray.push(Math.floor(Math.random()*100));
-    }
-    return mine;
+
+
+
+
+
+function arrayContain(num, array){
+    for (i = 0;  i < array.length; i++){
+        if (num == array[i]) {
+            return true
+        }  
+    }   
+    return false
 }
 
-function oneHun(minum, max) {
-    for ( minum = 1; minum <= max + 1; minum++){
-        oneToHun.push(minum);
+function flowers(sixTeen) {
+    for ( i = 1; i < sixTeen + 1; i++){
+        mineArray.push( random = Math.floor(Math.random()*100));
+        if ( random == random) {
+            random = Math.floor(Math.random()*100)
+        }
     }
-    return minum;
+    return mine;
 }
